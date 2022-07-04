2f211d403f55029ec9fe9a291787230a6d5de0ec
sushanshakya77 <sushanshakya77@gmail.com>
2022-07-04 13:19:50 +0545

    feat: added about page

---
+++ apps/frontend/src/app/Pages/About.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    // <div>
+    <section className="flex justify-center p-5 ">
+      <div className=" space-y-4 ">
+        <h2 className="text-3xl text-center text-[#45505b] font-semibold border-b-4 border-indigo-500 ">
+          ABOUT
+        </h2>
+        <p>
+          I'm a Web Developer with a passion for building web applications. I
+          have a background in Computer Science and a passion for learning new
+          technologies. I'm currently working as a Web Developer at{' '}
+          <a href="https://www.linkedin.com/in/sushan-shakya-b9a8b9a4/">
+            Linkedin
+          </a>
+        </p>
+      </div>
+    </section>
+    // </div>
+  );
+};
+
+export default About;
