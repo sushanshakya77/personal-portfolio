49ad7afac3ddad1d75349ae77dd6ea01b97e2a41
sushanshakya77 <sushanshakya77@gmail.com>
2022-07-04 11:15:07 +0545

    config: tailwind config

---
+++ apps/frontend/src/tailwind.config.js
@@ -0,0 +1,17 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
+const { join } = require('path');
+
+module.exports = {
+  mode: 'jit',
+  content: [
+    join(__dirname, '**/*.{js,jsx,ts,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
