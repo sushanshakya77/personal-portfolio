fdb90a25d9ed2383d78d520dde83a73fca130687
sushanshakya77 <sushanshakya77@gmail.com>
2022-07-09 17:10:33 +0545

    feat: added facts page

---
+++ apps/frontend/src/app/Pages/Facts.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Facts = () => {
+  return (
+    <section className="space-y-4 px-16 my-16">
+      <div className="space-y-4">
+        <h2 className="text-3xl text-center text-[#45505b] font-semibold border-b-2 border-indigo-500 p-3 ">
+          FACTS
+        </h2>
+        <p className="text-center">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam,
+          saepe numquam. Iusto, a voluptatibus, delectus architecto quia vitae
+          animi facere quo assumenda earum cupiditate. Est iste eveniet at
+          tenetur quae.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Facts;
