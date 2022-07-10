4b89b341f2087dab5d1fa25621207e6eaffe1db9
sushanshakya77 <sushanshakya77@gmail.com>
2022-07-10 19:35:06 +0545

    feat: add custom style for section

--- apps/frontend/src/app/Components/Section.tsx
+++ apps/frontend/src/app/Components/Section.tsx
@@ -2,10 +2,15 @@ import React, { ReactNode } from 'react';
 
 interface ISectionProps {
   children: ReactNode;
+  customStyle?: string;
 }
 
-const Section = ({ children }: ISectionProps) => {
-  return <section className="space-y-8 px-16 my-16">{children}</section>;
+const Section = ({ children, customStyle }: ISectionProps) => {
+  return (
+    <section className={`space-y-8 px-16 my-16 ${customStyle}`}>
+      {children}
+    </section>
+  );
 };
 
 export default Section;
