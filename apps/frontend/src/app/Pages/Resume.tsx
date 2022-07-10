8fbbef34640b0b448a898d0d2c760a11e6e9a5e9
sushanshakya77 <sushanshakya77@gmail.com>
2022-07-10 14:25:18 +0545

    feat: added resume section

---
+++ apps/frontend/src/app/Pages/Resume.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Section from '../Components/Section';
+import Title from '../Components/Title';
+
+const Resume = () => {
+  return (
+    <Section>
+      <Title
+        title="Resume"
+        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam,
+          saepe numquam. Iusto, a voluptatibus, delectus architecto quia vitae
+          animi facere quo assumenda earum cupiditate. Est iste eveniet at
+          tenetur quae."
+      />
+    </Section>
+  );
+};
+
+export default Resume;
