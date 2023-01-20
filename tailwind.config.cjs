5504749fe31d5c887b92ef3a6b910527aa489022
Sushan Shakya <sushanshakya77@gmail.com>
2023-01-20 12:40:03 +0545

    refactor: added animation style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,20 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fade: {
+          '0%': {
+            transform: 'translateY(25%)',
+            opacity: 0
+          },
+          "100%": {
+            transform: 'translateY(0%)',
+            opacity: 1
+          }
+        },
+      }
+    },
     fontFamily: {
       'sans-serif': ['Raleway', 'sans-serif'],
     },
