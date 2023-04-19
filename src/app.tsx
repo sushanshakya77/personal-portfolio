b34ca95bab2538d8c014fe9403f8fdcc5c2314bb
sushanshakya77 <sushanshakya77@gmail.com>
2023-04-19 20:52:22 +0545

    feat: added google analytics

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import Sidebar from "./Components/Sidebar";
 import Main from "./Pages/Main";
+import ReactGA from "react-ga";
 
 export function App() {
+  const TRACKING_ID = "UA-217843204-1";
+  ReactGA.initialize(TRACKING_ID);
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <>
       <Sidebar />
